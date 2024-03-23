397c73b9b484f242e78cd378c2fdaa88ee12f752
yangheqi <2378459785@qq.com>
2024-03-23 18:09:20 +0800

    rollback complete

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+let terminal = null;
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 
@@ -18,16 +18,73 @@ function activate(context) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('githelper.extension.rollback', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from GitHelper!');
+		// 获取当前活动文档
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return; // 没有打开的活动文档
+		}
+
+		// 获取活动文档的绝对路径
+		const documentPath = editor.document.uri.fsPath;
+		// 配置：是否在回滚时备份当前文档
+		const rollbackAutoSave = vscode.workspace.getConfiguration().get('skysource2030.rollbackAutoSave');
+		if (rollbackAutoSave) {
+			rollbackSaveBackup(documentPath);
+		}
+
+		// 执行git checkout HEAD命令
+		executeGitCommand(`git checkout HEAD '${documentPath}'`, "");
 	});
 
 	context.subscriptions.push(disposable);
+
+	// rollback回滚
+	async function executeGitCommand(command, options) {
+		if (!terminal) {
+			terminal = vscode.window.createTerminal(options);
+		}
+
+		terminal.sendText(command);
+	}
+
+	// 回滚时备份当前文档
+	async function rollbackSaveBackup(file) {
+		// 配置：备份路径
+		const rollbackAutoSavePath = vscode.workspace.getConfiguration().get('skysource2030.rollbackAutoSavePath');
+
+		// 检查备份路径是否为空
+		if (!rollbackAutoSavePath) {
+			vscode.window.showErrorMessage('请设置备份路径或关闭备份开关');
+			return;
+		}
+		// 如果没有路径就创建一个
+		const now = new Date();
+		const year = now.getFullYear();
+		const month = now.getMonth() + 1;
+		const day = now.getDate();
+		let date = year * 10000 + month * 100 + day;
+		
+		// 删除旧的备份
+		const pathDel1 = rollbackAutoSavePath + '\\' + (date - 2).toString();
+		const pathDel2 = rollbackAutoSavePath + '\\' + (date - 3).toString();
+		const delCommand = 'rm -rf \'' + pathDel1 + '\' \'' + pathDel2 + '\'';
+		terminal.sendText(delCommand);
+
+		// 创建备份目录
+		const pathBackup = rollbackAutoSavePath + '\\' + date.toString();
+		const mkdirCommand = 'mkdir -p \'' + pathBackup + '\'';
+		terminal.sendText(mkdirCommand);
+		// 备份当前文档
+		const  BackupCommand = 'cp \'' + file + '\' -t \'' + pathBackup + '\'';
+		terminal.sendText(BackupCommand);
+		vscode.window.showInformationMessage(`已备份当前文档至${pathBackup}`);
+
+		terminal.show();
+	}
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
